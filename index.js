f879a766e059a8a92f7bb155ebbe7784466a1711
Ally Netzel <netzelally@gmail.com>
2018-12-01 23:59:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+class Driver {
+    constructor(trips, passengers) {
+        this.trips = trips;
+        this.passengers = passengers;
+ 
+    }
+
+    newDriver(name) {
+    return this.name = name;
+    }
+}
